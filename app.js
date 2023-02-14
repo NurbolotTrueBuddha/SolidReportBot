13174df200decccc91111495cefecc9ebf4c81bf
buddha <zzhaparovn@gmail.com>
2023-02-14 19:19:30 +0600

    123

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import Validate from './validate.js'
 import Handler from './handler.js';
-const token = '6127897079:AAEkAC_N8ek6tAIyWL0wNMlCJ9Lcgd1Kw04';
+const token = '';
 const bot = new TelegramBot(token, { polling: true });
 let handler = new Handler(bot);
 let validate = new Validate(handler);
